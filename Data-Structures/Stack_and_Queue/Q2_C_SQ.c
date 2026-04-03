b3889eadfe774593a6eee0eb273114ca3de50949
choi woo-nyong <woonyong.kr@gmail.com>
2026-04-04 00:56:34 +0900

    feat(stack-queue): Q2_C_SQ 업데이트

--- Data-Structures/Stack_and_Queue/Q2_C_SQ.c
+++ Data-Structures/Stack_and_Queue/Q2_C_SQ.c
@@ -1,8 +1,8 @@
 //////////////////////////////////////////////////////////////////////////////////
 
-/* CE1007/CZ1007 Data Structures
-Lab Test: Section C - Stack and Queue Questions
-Purpose: Implementing the required functions for Question 2 */
+/* CE1007/CZ1007 자료구조
+랩 테스트: 섹션 C - 스택 및 큐 문제
+목적: 문제 2에 필요한 함수를 구현하기 */
 
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -16,22 +16,22 @@ typedef struct _listnode
 {
 	int item;
 	struct _listnode *next;
-} ListNode;	// You should not change the definition of ListNode
+} ListNode;	// ListNode의 정의는 변경하면 안 됩니다
 
 typedef struct _linkedlist
 {
 	int size;
 	ListNode *head;
-} LinkedList;	// You should not change the definition of LinkedList
+} LinkedList;	// LinkedList의 정의는 변경하면 안 됩니다
 
 typedef struct _stack
 {
 	LinkedList ll;
-}Stack;  // You should not change the definition of Stack
+}Stack;  // Stack의 정의는 변경하면 안 됩니다
 
-///////////////////////// function prototypes ////////////////////////////////////
+///////////////////////// 함수 원형 ////////////////////////////////////
 
-// You should not change the prototypes of these functions
+// 이 함수들의 원형은 변경하면 안 됩니다
 void createStackFromLinkedList(LinkedList *ll , Stack *stack);
 void removeEvenValues(Stack *s);
 
@@ -55,41 +55,41 @@ int main()
 	Stack s;
 
 	c = 1;
-	// Initialize the linked list as an empty linked list
+	// 연결 리스트를 빈 연결 리스트로 초기화
 	ll.head = NULL;
 	ll.size = 0;
 
-	// Initalize the stack as an empty stack
+	// 스택을 빈 스택으로 초기화
 	s.ll.head = NULL;
 	s.ll.size = 0;
 
-	printf("1: Insert an integer into the linked list:\n");
-	printf("2: Create the stack from the linked list:\n");
-	printf("3: Remove even numbers from the stack:\n");
-	printf("0: Quit:\n");
+	printf("1: 연결 리스트에 정수를 삽입:\n");
+	printf("2: 연결 리스트로부터 스택을 생성:\n");
+	printf("3: 스택에서 짝수를 제거:\n");
+	printf("0: 종료:\n");
 
 	while (c != 0)
 	{
-		printf("Please input your choice(1/2/3/0): ");
+		printf("원하는 작업을 입력하세요(1/2/3/0): ");
 		scanf("%d", &c);
 
 		switch (c)
 		{
 		case 1:
-			printf("Input an integer that you want to add to the linked list: ");
+			printf("연결 리스트에 추가할 정수를 입력하세요: ");
 			scanf("%d", &i);
 			insertNode(&ll, ll.size, i);
-			printf("The resulting linked list is: ");
+			printf("결과 연결 리스트: ");
 			printList(&ll);
 			break;
 		case 2:
-			createStackFromLinkedList(&ll, &s); // You need to code this function
-			printf("The resulting stack is: ");
+			createStackFromLinkedList(&ll, &s); // 이 함수는 직접 구현해야 합니다
+			printf("결과 스택: ");
 			printList(&(s.ll));
 			break;
 		case 3:
-			removeEvenValues(&s); // You need to code this function
-			printf("The resulting stack after removing even integers is: ");
+			removeEvenValues(&s); // 이 함수는 직접 구현해야 합니다
+			printf("짝수를 제거한 뒤의 스택: ");
 			printList(&(s.ll));
 			removeAllItemsFromStack(&s);
 			removeAllItems(&ll);
@@ -99,7 +99,7 @@ int main()
 			removeAllItems(&ll);
 			break;
 		default:
-			printf("Choice unknown;\n");
+			printf("알 수 없는 선택입니다.\n");
 			break;
 		}
 
@@ -113,12 +113,12 @@ int main()
 
 void createStackFromLinkedList(LinkedList *ll, Stack *s)
 {
-    /* add your code here */
+    /* 여기에 코드를 작성하세요 */
 }
 
 void removeEvenValues(Stack *s)
 {
-	/* add your code here */
+	/* 여기에 코드를 작성하세요 */
 }
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -170,7 +170,7 @@ void printList(LinkedList *ll){
 		return;
 	cur = ll->head;
 	if (cur == NULL)
-		printf("Empty");
+		printf("비어 있음");
 	while (cur != NULL)
 	{
 		printf("%d ", cur->item);
@@ -224,7 +224,7 @@ int insertNode(LinkedList *ll, int index, int value){
 	if (ll == NULL || index < 0 || index > ll->size + 1)
 		return -1;
 
-	// If empty list or inserting first node, need to update head pointer
+	// 빈 리스트이거나 첫 번째 노드를 삽입하는 경우 head 포인터를 갱신해야 함
 	if (ll->head == NULL || index == 0){
 		cur = ll->head;
 		ll->head = malloc(sizeof(ListNode));
@@ -239,8 +239,8 @@ int insertNode(LinkedList *ll, int index, int value){
 	}
 
 
-	// Find the nodes before and at the target position
-	// Create a new node and reconnect the links
+	// 목표 위치의 이전 노드와 해당 위치의 노드를 찾기
+	// 새 노드를 만들고 링크를 다시 연결하기
 	if ((pre = findNode(ll, index - 1)) != NULL){
 		cur = pre->next;
 		pre->next = malloc(sizeof(ListNode));
@@ -262,11 +262,11 @@ int removeNode(LinkedList *ll, int index){
 
 	ListNode *pre, *cur;
 
-	// Highest index we can remove is size-1
+	// 제거할 수 있는 가장 큰 인덱스는 size-1임
 	if (ll == NULL || index < 0 || index >= ll->size)
 		return -1;
 
-	// If removing first node, need to update head pointer
+	// 첫 번째 노드를 제거하는 경우 head 포인터를 갱신해야 함
 	if (index == 0){
 		cur = ll->head->next;
 		free(ll->head);
@@ -275,8 +275,8 @@ int removeNode(LinkedList *ll, int index){
 		return 0;
 	}
 
-	// Find the nodes before and after the target position
-	// Free the target node and reconnect the links
+	// 목표 위치의 이전 노드와 다음 노드를 찾기
+	// 대상 노드를 해제하고 링크를 다시 연결하기
 	if ((pre = findNode(ll, index - 1)) != NULL){
 
 		if (pre->next == NULL)

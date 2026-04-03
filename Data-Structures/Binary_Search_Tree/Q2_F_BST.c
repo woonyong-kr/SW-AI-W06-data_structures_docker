e89acc5f9578d6228d1f5d3cf214f0304eb711bc
choi woo-nyong <woonyong.kr@gmail.com>
2026-04-04 00:56:35 +0900

    feat(binary-search-tree): Q2_F_BST 업데이트

--- Data-Structures/Binary_Search_Tree/Q2_F_BST.c
+++ Data-Structures/Binary_Search_Tree/Q2_F_BST.c
@@ -1,8 +1,8 @@
 //////////////////////////////////////////////////////////////////////////////////
 
-/* CE1007/CZ1007 Data Structures
-Lab Test: Section F - Binary Search Trees Questions
-Purpose: Implementing the required functions for Question 2 */
+/* CE1007/CZ1007 자료구조
+랩 테스트: 섹션 F - 이진 탐색 트리 문제
+목적: 문제 2에 필요한 함수를 구현하기 */
 
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -15,21 +15,21 @@ typedef struct _bstnode{
 	int item;
 	struct _bstnode *left;
 	struct _bstnode *right;
-} BSTNode;   // You should not change the definition of BSTNode
+} BSTNode;   // BSTNode의 정의는 변경하면 안 됩니다
 
 typedef struct _stackNode{
 	BSTNode *data;
 	struct _stackNode *next;
-}StackNode; // You should not change the definition of StackNode
+}StackNode; // StackNode의 정의는 변경하면 안 됩니다
 
 typedef struct _stack
 {
 	StackNode *top;
-}Stack; // You should not change the definition of Stack
+}Stack; // Stack의 정의는 변경하면 안 됩니다
 
-///////////////////////// function prototypes ////////////////////////////////////
+///////////////////////// 함수 원형 ////////////////////////////////////
 
-// You should not change the prototypes of these functions
+// 이 함수들의 원형은 변경하면 안 됩니다
 void inOrderTraversal(BSTNode *node);
 
 void insertBSTNode(BSTNode **node, int value);
@@ -47,37 +47,37 @@ int main()
 	int c, i;
 	c = 1;
 
-	//Initialize the Binary Search Tree as an empty Binary Search Tree
+	// 이진 탐색 트리를 빈 이진 탐색 트리로 초기화
 	BSTNode *root;
 	root = NULL;
 
-	printf("1: Insert an integer into the binary search tree;\n");
-	printf("2: Print the in-order traversal of the binary search tree;\n");
-	printf("0: Quit;\n");
+	printf("1: 이진 탐색 트리에 정수를 삽입;\n");
+	printf("2: 이진 탐색 트리의 중위 순회 결과를 출력;\n");
+	printf("0: 종료;\n");
 
 
 	while (c != 0)
 	{
-		printf("Please input your choice(1/2/0): ");
+		printf("원하는 작업을 입력하세요(1/2/0): ");
 		scanf("%d", &c);
 
 		switch (c)
 		{
 		case 1:
-			printf("Input an integer that you want to insert into the Binary Search Tree: ");
+			printf("이진 탐색 트리에 삽입할 정수를 입력하세요: ");
 			scanf("%d", &i);
 			insertBSTNode(&root, i);
 			break;
 		case 2:
-			printf("The resulting in-order traversal of the binary search tree is: ");
-			inOrderTraversal(root); // You need to code this function
+			printf("이진 탐색 트리의 중위 순회 결과: ");
+			inOrderTraversal(root); // 이 함수는 직접 구현해야 합니다
 			printf("\n");
 			break;
 		case 0:
 			removeAll(&root);
 			break;
 		default:
-			printf("Choice unknown;\n");
+			printf("알 수 없는 선택입니다.\n");
 			break;
 		}
 
@@ -90,7 +90,7 @@ int main()
 
 void inOrderTraversal(BSTNode *root)
 {
-	 /* add your code here */
+	 /* 여기에 코드를 작성하세요 */
 }
 
 ///////////////////////////////////////////////////////////////////////////////

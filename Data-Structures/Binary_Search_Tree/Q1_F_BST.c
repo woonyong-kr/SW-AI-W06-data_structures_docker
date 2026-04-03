03393c155033d2d5a6e788e0893018c16b541a10
choi woo-nyong <woonyong.kr@gmail.com>
2026-04-04 00:56:35 +0900

    feat(binary-search-tree): Q1_F_BST 업데이트

--- Data-Structures/Binary_Search_Tree/Q1_F_BST.c
+++ Data-Structures/Binary_Search_Tree/Q1_F_BST.c
@@ -1,9 +1,9 @@
 
 //////////////////////////////////////////////////////////////////////////////////
 
-/* CE1007/CZ1007 Data Structures
-Lab Test: Section F - Binary Search Trees Questions
-Purpose: Implementing the required functions for Question 1 */
+/* CE1007/CZ1007 자료구조
+랩 테스트: 섹션 F - 이진 탐색 트리 문제
+목적: 문제 1에 필요한 함수를 구현하기 */
 
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -17,23 +17,23 @@ typedef struct _bstnode{
 	int item;
 	struct _bstnode *left;
 	struct _bstnode *right;
-} BSTNode;   // You should not change the definition of BSTNode
+} BSTNode;   // BSTNode의 정의는 변경하면 안 됩니다
 
 typedef struct _QueueNode {
 	BSTNode *data;
 	struct _QueueNode *nextPtr;
-}QueueNode; // You should not change the definition of QueueNode
+}QueueNode; // QueueNode의 정의는 변경하면 안 됩니다
 
 
 typedef struct _queue
 {
 	QueueNode *head;
 	QueueNode *tail;
-}Queue; // You should not change the definition of queue
+}Queue; // queue의 정의는 변경하면 안 됩니다
 
 ///////////////////////////////////////////////////////////////////////////////////
 
-// You should not change the prototypes of these functions
+// 이 함수들의 원형은 변경하면 안 됩니다
 void levelOrderTraversal(BSTNode *node);
 
 void insertBSTNode(BSTNode **node, int value);
@@ -50,37 +50,37 @@ int main()
 	int c, i;
 	c = 1;
 
-	//Initialize the Binary Search Tree as an empty Binary Search Tree
+	// 이진 탐색 트리를 빈 이진 탐색 트리로 초기화
 	BSTNode *root;
 	root = NULL;
 
-	printf("1: Insert an integer into the binary search tree;\n");
-	printf("2: Print the level-order traversal of the binary search tree;\n");
-	printf("0: Quit;\n");
+	printf("1: 이진 탐색 트리에 정수를 삽입;\n");
+	printf("2: 이진 탐색 트리의 레벨 순회 결과를 출력;\n");
+	printf("0: 종료;\n");
 
 
 	while (c != 0)
 	{
-		printf("Please input your choice(1/2/0): ");
+		printf("원하는 작업을 입력하세요(1/2/0): ");
 		scanf("%d", &c);
 
 		switch (c)
 		{
 		case 1:
-			printf("Input an integer that you want to insert into the Binary Search Tree: ");
+			printf("이진 탐색 트리에 삽입할 정수를 입력하세요: ");
 			scanf("%d", &i);
 			insertBSTNode(&root, i);
 			break;
 		case 2:
-			printf("The resulting level-order traversal of the binary search tree is: ");
-			levelOrderTraversal(root); // You need to code this function
+			printf("이진 탐색 트리의 레벨 순회 결과: ");
+			levelOrderTraversal(root); // 이 함수는 직접 구현해야 합니다
 			printf("\n");
 			break;
 		case 0:
 			removeAll(&root);
 			break;
 		default:
-			printf("Choice unknown;\n");
+			printf("알 수 없는 선택입니다.\n");
 			break;
 		}
 
@@ -94,7 +94,7 @@ int main()
 void levelOrderTraversal(BSTNode* root)
 {
 
-    /* add your code here */
+    /* 여기에 코드를 작성하세요 */
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -127,29 +127,29 @@ void insertBSTNode(BSTNode **node, int value){
 
 //////////////////////////////////////////////////////////////////////////////////
 
-// enqueue node
+// 노드를 큐에 삽입
 void enqueue(QueueNode **headPtr, QueueNode **tailPtr, BSTNode *node)
 {
-	// dynamically allocate memory
+	// 메모리를 동적으로 할당
 	QueueNode *newPtr = malloc(sizeof(QueueNode));
 
-	// if newPtr does not equal NULL
+	// newPtr가 NULL이 아닌 경우
 	if (newPtr != NULL) {
 		newPtr->data = node;
 		newPtr->nextPtr = NULL;
 
-		// if queue is empty, insert at head
+		// 큐가 비어 있으면 head에 삽입
 		if (isEmpty(*headPtr)) {
 			*headPtr = newPtr;
 		}
-		else { // insert at tail
+		else { // tail에 삽입
 			(*tailPtr)->nextPtr = newPtr;
 		}
 
 		*tailPtr = newPtr;
 	}
 	else {
-		printf("Node not inserted");
+		printf("노드가 삽입되지 않았습니다");
 	}
 }
 

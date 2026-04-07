81466ea0edf137b28cd38b42bc907c5664adfbc6
choi woo-nyong <woonyong.kr@gmail.com>
2026-04-07 20:46:52 +0900

    feat(stack-queue): Q2 스택 생성과 짝수 제거 구현

--- Data-Structures/Stack_and_Queue/Q2_C_SQ.c
+++ Data-Structures/Stack_and_Queue/Q2_C_SQ.c
@@ -12,284 +12,270 @@
 #define MIN_INT -1000
 //////////////////////////////////////////////////////////////////////////////////
 
-typedef struct _listnode
-{
-	int item;
-	struct _listnode *next;
-} ListNode;	// ListNode의 정의는 변경하면 안 됩니다
+typedef struct _listnode {
+  int item;
+  struct _listnode* next;
+} ListNode;  // ListNode의 정의는 변경하면 안 됩니다
 
-typedef struct _linkedlist
-{
-	int size;
-	ListNode *head;
-} LinkedList;	// LinkedList의 정의는 변경하면 안 됩니다
+typedef struct _linkedlist {
+  int size;
+  ListNode* head;
+} LinkedList;  // LinkedList의 정의는 변경하면 안 됩니다
 
-typedef struct _stack
-{
-	LinkedList ll;
-}Stack;  // Stack의 정의는 변경하면 안 됩니다
+typedef struct _stack {
+  LinkedList ll;
+} Stack;  // Stack의 정의는 변경하면 안 됩니다
 
 ///////////////////////// 함수 원형 ////////////////////////////////////
 
 // 이 함수들의 원형은 변경하면 안 됩니다
-void createStackFromLinkedList(LinkedList *ll , Stack *stack);
-void removeEvenValues(Stack *s);
+void createStackFromLinkedList(LinkedList* ll, Stack* stack);
+void removeEvenValues(Stack* s);
 
-void push(Stack *s , int item);
-int pop(Stack *s);
-int isEmptyStack(Stack *s);
-void removeAllItemsFromStack(Stack *s);
+void push(Stack* s, int item);
+int pop(Stack* s);
+int isEmptyStack(Stack* s);
+void removeAllItemsFromStack(Stack* s);
 
-void printList(LinkedList *ll);
-ListNode * findNode(LinkedList *ll, int index);
-int insertNode(LinkedList *ll, int index, int value);
-int removeNode(LinkedList *ll, int index);
-void removeAllItems(LinkedList *ll);
+void printList(LinkedList* ll);
+ListNode* findNode(LinkedList* ll, int index);
+int insertNode(LinkedList* ll, int index, int value);
+int removeNode(LinkedList* ll, int index);
+void removeAllItems(LinkedList* ll);
 
-//////////////////////////// main() //////////////////////////////////////////////
+//////////////////////////// main()
+/////////////////////////////////////////////////
 
-int main()
-{
-	int c, i;
-	LinkedList ll;
-	Stack s;
+int main() {
+  int c, i;
+  LinkedList ll;
+  Stack s;
 
-	c = 1;
-	// 연결 리스트를 빈 연결 리스트로 초기화
-	ll.head = NULL;
-	ll.size = 0;
+  c = 1;
+  // 연결 리스트를 빈 연결 리스트로 초기화
+  ll.head = NULL;
+  ll.size = 0;
 
-	// 스택을 빈 스택으로 초기화
-	s.ll.head = NULL;
-	s.ll.size = 0;
+  // 스택을 빈 스택으로 초기화
+  s.ll.head = NULL;
+  s.ll.size = 0;
 
-	printf("1: 연결 리스트에 정수를 삽입:\n");
-	printf("2: 연결 리스트로부터 스택을 생성:\n");
-	printf("3: 스택에서 짝수를 제거:\n");
-	printf("0: 종료:\n");
+  printf("1: 연결 리스트에 정수를 삽입:\n");
+  printf("2: 연결 리스트로부터 스택을 생성:\n");
+  printf("3: 스택에서 짝수를 제거:\n");
+  printf("0: 종료:\n");
 
-	while (c != 0)
-	{
-		printf("원하는 작업을 입력하세요(1/2/3/0): ");
-		scanf("%d", &c);
+  while (c != 0) {
+    printf("원하는 작업을 입력하세요(1/2/3/0): ");
+    scanf("%d", &c);
 
-		switch (c)
-		{
-		case 1:
-			printf("연결 리스트에 추가할 정수를 입력하세요: ");
-			scanf("%d", &i);
-			insertNode(&ll, ll.size, i);
-			printf("결과 연결 리스트: ");
-			printList(&ll);
-			break;
-		case 2:
-			createStackFromLinkedList(&ll, &s); // 이 함수는 직접 구현해야 합니다
-			printf("결과 스택: ");
-			printList(&(s.ll));
-			break;
-		case 3:
-			removeEvenValues(&s); // 이 함수는 직접 구현해야 합니다
-			printf("짝수를 제거한 뒤의 스택: ");
-			printList(&(s.ll));
-			removeAllItemsFromStack(&s);
-			removeAllItems(&ll);
-			break;
-		case 0:
-			removeAllItemsFromStack(&s);
-			removeAllItems(&ll);
-			break;
-		default:
-			printf("알 수 없는 선택입니다.\n");
-			break;
-		}
+    switch (c) {
+      case 1:
+        printf("연결 리스트에 추가할 정수를 입력하세요: ");
+        scanf("%d", &i);
+        insertNode(&ll, ll.size, i);
+        printf("결과 연결 리스트: ");
+        printList(&ll);
+        break;
+      case 2:
+        createStackFromLinkedList(&ll, &s);  // 이 함수는 직접 구현해야 합니다
+        printf("결과 스택: ");
+        printList(&(s.ll));
+        break;
+      case 3:
+        removeEvenValues(&s);  // 이 함수는 직접 구현해야 합니다
+        printf("짝수를 제거한 뒤의 스택: ");
+        printList(&(s.ll));
+        removeAllItemsFromStack(&s);
+        removeAllItems(&ll);
+        break;
+      case 0:
+        removeAllItemsFromStack(&s);
+        removeAllItems(&ll);
+        break;
+      default:
+        printf("알 수 없는 선택입니다.\n");
+        break;
+    }
+  }
 
-	}
-
-	return 0;
-}
-
-
-//////////////////////////////////////////////////////////////////////////////////
-void createStackFromLinkedList(LinkedList *ll, Stack *s)
-{
-    // Todo: 연결 리스트의 값을 이용해 스택을 구성하세요.
-    // 문제의 스택 순서가 맞도록 push를 활용하고, 스택 내부 연결 리스트와 size가 올바르게 설정되도록 하세요.
-    /* 여기에 코드를 작성하세요 */
-}
-void removeEvenValues(Stack *s)
-{
-	// Todo: 스택에서 짝수 값만 제거하세요.
-	// 제거 후 남은 원소들의 순서가 문제 의도에 맞게 유지되도록 보조 스택 또는 적절한 로직을 사용하세요.
-	/* 여기에 코드를 작성하세요 */
+  return 0;
 }
 
 //////////////////////////////////////////////////////////////////////////////////
+void createStackFromLinkedList(LinkedList* ll, Stack* s) {
+  // Todo: 연결 리스트의 값을 이용해 스택을 구성하세요.
+  // 문제의 스택 순서가 맞도록 push를 활용하고, 스택 내부 연결 리스트와 size가
+  // 올바르게 설정되도록 하세요.
 
-void push(Stack *s, int item)
-{
-	insertNode(&(s->ll), 0, item);
+  if (ll == NULL || s == NULL) return;
+
+  removeAllItemsFromStack(s);
+
+  ListNode* curr = ll->head;
+
+  while (curr != NULL) {
+    push(s, curr->item);
+    curr = curr->next;
+  }
+}
+void removeEvenValues(Stack* s) {
+  // Todo: 스택에서 짝수 값만 제거하세요.
+  // 제거 후 남은 원소들의 순서가 문제 의도에 맞게 유지되도록 보조 스택 또는
+  // 적절한 로직을 사용하세요.
+
+  if (s == NULL) return;
+
+  Stack temp;
+  temp.ll.head = NULL;
+  temp.ll.size = 0;
+
+  while (!isEmptyStack(s)) {
+    int value = pop(s);
+    if (value % 2 != 0) {
+      push(&temp, value);
+    }
+  }
+
+  while (!isEmptyStack(&temp)) {
+    push(s, pop(&temp));
+  }
 }
 
-int pop(Stack *s)
-{
-	int item;
-	if (s->ll.head != NULL)
-	{
-		item = ((s->ll).head)->item;
-		removeNode(&(s->ll), 0);
-		return item;
-	}
-	else
-		return MIN_INT;
+//////////////////////////////////////////////////////////////////////////////////
+
+void push(Stack* s, int item) { insertNode(&(s->ll), 0, item); }
+
+int pop(Stack* s) {
+  int item;
+  if (s->ll.head != NULL) {
+    item = ((s->ll).head)->item;
+    removeNode(&(s->ll), 0);
+    return item;
+  } else
+    return MIN_INT;
 }
 
-int isEmptyStack(Stack *s)
-{
-	if ((s->ll).size == 0)
-		return 1;
-	else
-		return 0;
+int isEmptyStack(Stack* s) {
+  if ((s->ll).size == 0)
+    return 1;
+  else
+    return 0;
 }
 
-
-void removeAllItemsFromStack(Stack *s)
-{
-	if (s == NULL)
-		return;
-	while (s->ll.head != NULL)
-	{
-		pop(s);
-	}
+void removeAllItemsFromStack(Stack* s) {
+  if (s == NULL) return;
+  while (s->ll.head != NULL) {
+    pop(s);
+  }
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
-
-void printList(LinkedList *ll){
-
-	ListNode *cur;
-	if (ll == NULL)
-		return;
-	cur = ll->head;
-	if (cur == NULL)
-		printf("비어 있음");
-	while (cur != NULL)
-	{
-		printf("%d ", cur->item);
-		cur = cur->next;
-	}
-	printf("\n");
+void printList(LinkedList* ll) {
+  ListNode* cur;
+  if (ll == NULL) return;
+  cur = ll->head;
+  if (cur == NULL) printf("비어 있음");
+  while (cur != NULL) {
+    printf("%d ", cur->item);
+    cur = cur->next;
+  }
+  printf("\n");
 }
 
+void removeAllItems(LinkedList* ll) {
+  ListNode* cur = ll->head;
+  ListNode* tmp;
 
-void removeAllItems(LinkedList *ll)
-{
-	ListNode *cur = ll->head;
-	ListNode *tmp;
-
-	while (cur != NULL){
-		tmp = cur->next;
-		free(cur);
-		cur = tmp;
-	}
-	ll->head = NULL;
-	ll->size = 0;
+  while (cur != NULL) {
+    tmp = cur->next;
+    free(cur);
+    cur = tmp;
+  }
+  ll->head = NULL;
+  ll->size = 0;
 }
 
+ListNode* findNode(LinkedList* ll, int index) {
+  ListNode* temp;
 
-ListNode * findNode(LinkedList *ll, int index){
+  if (ll == NULL || index < 0 || index >= ll->size) return NULL;
 
-	ListNode *temp;
+  temp = ll->head;
 
-	if (ll == NULL || index < 0 || index >= ll->size)
-		return NULL;
+  if (temp == NULL || index < 0) return NULL;
 
-	temp = ll->head;
+  while (index > 0) {
+    temp = temp->next;
+    if (temp == NULL) return NULL;
+    index--;
+  }
 
-	if (temp == NULL || index < 0)
-		return NULL;
-
-	while (index > 0){
-		temp = temp->next;
-		if (temp == NULL)
-			return NULL;
-		index--;
-	}
-
-	return temp;
+  return temp;
 }
 
-int insertNode(LinkedList *ll, int index, int value){
+int insertNode(LinkedList* ll, int index, int value) {
+  ListNode *pre, *cur;
 
-	ListNode *pre, *cur;
+  if (ll == NULL || index < 0 || index > ll->size + 1) return -1;
 
-	if (ll == NULL || index < 0 || index > ll->size + 1)
-		return -1;
+  // 빈 리스트이거나 첫 번째 노드를 삽입하는 경우 head 포인터를 갱신해야 함
+  if (ll->head == NULL || index == 0) {
+    cur = ll->head;
+    ll->head = malloc(sizeof(ListNode));
+    if (ll->head == NULL) {
+      exit(0);
+    }
+    ll->head->item = value;
+    ll->head->next = cur;
+    ll->size++;
+    return 0;
+  }
 
-	// 빈 리스트이거나 첫 번째 노드를 삽입하는 경우 head 포인터를 갱신해야 함
-	if (ll->head == NULL || index == 0){
-		cur = ll->head;
-		ll->head = malloc(sizeof(ListNode));
-		if (ll->head == NULL)
-		{
-			exit(0);
-		}
-		ll->head->item = value;
-		ll->head->next = cur;
-		ll->size++;
-		return 0;
-	}
+  // 목표 위치의 이전 노드와 해당 위치의 노드를 찾기
+  // 새 노드를 만들고 링크를 다시 연결하기
+  if ((pre = findNode(ll, index - 1)) != NULL) {
+    cur = pre->next;
+    pre->next = malloc(sizeof(ListNode));
+    if (pre->next == NULL) {
+      exit(0);
+    }
+    pre->next->item = value;
+    pre->next->next = cur;
+    ll->size++;
+    return 0;
+  }
 
-
-	// 목표 위치의 이전 노드와 해당 위치의 노드를 찾기
-	// 새 노드를 만들고 링크를 다시 연결하기
-	if ((pre = findNode(ll, index - 1)) != NULL){
-		cur = pre->next;
-		pre->next = malloc(sizeof(ListNode));
-		if (pre->next == NULL)
-		{
-			exit(0);
-		}
-		pre->next->item = value;
-		pre->next->next = cur;
-		ll->size++;
-		return 0;
-	}
-
-	return -1;
+  return -1;
 }
 
+int removeNode(LinkedList* ll, int index) {
+  ListNode *pre, *cur;
 
-int removeNode(LinkedList *ll, int index){
+  // 제거할 수 있는 가장 큰 인덱스는 size-1임
+  if (ll == NULL || index < 0 || index >= ll->size) return -1;
 
-	ListNode *pre, *cur;
+  // 첫 번째 노드를 제거하는 경우 head 포인터를 갱신해야 함
+  if (index == 0) {
+    cur = ll->head->next;
+    free(ll->head);
+    ll->head = cur;
+    ll->size--;
+    return 0;
+  }
 
-	// 제거할 수 있는 가장 큰 인덱스는 size-1임
-	if (ll == NULL || index < 0 || index >= ll->size)
-		return -1;
+  // 목표 위치의 이전 노드와 다음 노드를 찾기
+  // 대상 노드를 해제하고 링크를 다시 연결하기
+  if ((pre = findNode(ll, index - 1)) != NULL) {
+    if (pre->next == NULL) return -1;
 
-	// 첫 번째 노드를 제거하는 경우 head 포인터를 갱신해야 함
-	if (index == 0){
-		cur = ll->head->next;
-		free(ll->head);
-		ll->head = cur;
-		ll->size--;
-		return 0;
-	}
+    cur = pre->next;
+    pre->next = cur->next;
+    free(cur);
+    ll->size--;
+    return 0;
+  }
 
-	// 목표 위치의 이전 노드와 다음 노드를 찾기
-	// 대상 노드를 해제하고 링크를 다시 연결하기
-	if ((pre = findNode(ll, index - 1)) != NULL){
-
-		if (pre->next == NULL)
-			return -1;
-
-		cur = pre->next;
-		pre->next = cur->next;
-		free(cur);
-		ll->size--;
-		return 0;
-	}
-
-	return -1;
+  return -1;
 }
